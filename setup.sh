aa3ebd48ccce43dc0a1261ddaa9a80074329f974
Patrick Musau <pmusau13ster@gmail.com>
2019-12-10 16:28:04 -0600

    Added setup file

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#install all of the relevant ROS packages needed to run the simulator
+
+sudo apt-get install -y ros-kinetic-ros-control ros-kinetic-ros-controllers ros-kinetic-gazebo-ros-control ros-kinetic-ackermann-msgs ros-kinetic-joy
+sudo apt-get install -y ros-kinetic-teb-local-planner ros-kinetic-move-base ros-kinetic-navigation ros-kinetic-hector-slam
+
+pip install -r requirements.txt
