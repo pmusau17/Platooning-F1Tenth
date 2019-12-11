ad8c1cb986fb5ebd109bc2b7912e15af7759c845
Patrick Musau <patrick.musau@vanderbilt.edu>
2019-12-11 10:43:26 -0600

    Edited Requirements

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #install all of the relevant ROS packages needed to run the simulator
-
+sudo apt-get update
 sudo apt-get install -y ros-kinetic-ros-control ros-kinetic-ros-controllers ros-kinetic-gazebo-ros-control ros-kinetic-ackermann-msgs ros-kinetic-joy
 sudo apt-get install -y ros-kinetic-teb-local-planner ros-kinetic-move-base ros-kinetic-navigation ros-kinetic-hector-slam ros-kinetic-driver-common
 
