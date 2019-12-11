af8012abaafcf3a833c809fa446214d66c71c3fd
Patrick Musau <patrick.musau@vanderbilt.edu>
2019-12-11 10:58:28 -0600

    added another ros-dependency

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 #install all of the relevant ROS packages needed to run the simulator
 sudo apt-get update
 sudo apt-get install -y ros-kinetic-ros-control ros-kinetic-ros-controllers ros-kinetic-gazebo-ros-control ros-kinetic-ackermann-msgs ros-kinetic-joy
-sudo apt-get install -y ros-kinetic-teb-local-planner ros-kinetic-move-base ros-kinetic-navigation ros-kinetic-hector-slam ros-kinetic-driver-common
+sudo apt-get install -y ros-kinetic-teb-local-planner ros-kinetic-move-base ros-kinetic-navigation ros-kinetic-hector-slam ros-kinetic-driver-common ros-kinetic-actionlib 
 
 pip install rospkg
 pip uninstall em
