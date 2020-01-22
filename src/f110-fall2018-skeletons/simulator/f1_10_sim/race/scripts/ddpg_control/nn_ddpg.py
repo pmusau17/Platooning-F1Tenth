b4eedee12a3274fcd1f1c805a5318fbe799a180c
Nate Hamilton <nathaniel_hamilton@outlook.com>
2020-01-22 13:06:26 -0600

    completed implementation of DDPG NNs

--- src/f110-fall2018-skeletons/simulator/f1_10_sim/race/scripts/ddpg_control/nn_ddpg.py
+++ src/f110-fall2018-skeletons/simulator/f1_10_sim/race/scripts/ddpg_control/nn_ddpg.py
@@ -16,6 +16,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
+import torch.nn.functional as F
 
 # Set the random seed
 seed_number = 8
@@ -25,117 +26,180 @@ torch.cuda.manual_seed_all(seed_number)
 
 
 def init_weights(m):
+    """
+    Function for initializing layers with orthogonal weights.
+
+    :param m: (tensor)  the layer to be orthogonally weighted.
+    """
     if type(m) == nn.Linear:
         torch.nn.init.orthogonal_(m.weight)
 
 
+def fan_in_uniform_init(m):
+    """
+    Initializes the given layer uniformly, as prescribed in the DDPG paper
+
+    :param m: (tensor) The layer to be uniformly weighted.
+    """
+    weight_range = 1.0 / np.sqrt(m.size(-1))
+    nn.init.uniform_(m, -weight_range, weight_range)
+
+
 ########################################################################################################################
 class ActorNN(nn.Module):
-    def __init__(self, input_size=4, hidden_size=6, output_size=4):
+    def __init__(self, num_inputs=4, hidden_size1=400, hidden_size2=300, num_actions=2, final_bias=3e-3):
         super(ActorNN, self).__init__()
         """
-        This Neural Network architecture creates a full actor, which provides the control output.
+        This Neural Network architecture creates a full actor, which provides the control output. The architecture is 
+        derived from the original DDPG paper.
 
-        :param input_size:  (int)   The desired size of the input layer. Should be the same size as the number of 
+        :param num_inputs:  (int)   The desired size of the input layer. Should be the same size as the number of 
                                         inputs to the NN. Default=4
-        :param hidden_size: (int)   The desired size of the hidden layers. Default=6
-        :param output_size: (int)   The desired size of the output layer. Should be the same size as the number of 
-                                        outputs from the NN. Default=4
+        :param hidden_size1:(int)   The desired size of the first hidden layer. Default=400
+        :param hidden_size2:(int)   The desired size of the second hidden layer. Default=300
+        :param num_actions: (int)   The desired size of the output layer. Should be the same size as the number of 
+                                        outputs from the NN. Default=2
+        :param final_bias:  (float) The final layers' weight and bias range for uniform distribution. Default=3e-3
         """
-        # Save the inputs
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
 
-        # Initialize the body
-        self.body = BodyNN(input_size=self.input_size, hidden_size=self.hidden_size)
+        # The first layer
+        self.linear1 = nn.Linear(num_inputs, hidden_size1)
+        self.ln1 = nn.LayerNorm(hidden_size1)
 
-        # Create the output layer
-        self.fc_control = nn.Sequential(
-            nn.Linear(in_features=self.hidden_size, out_features=self.output_size)
-        )
+        # The second layer
+        self.linear2 = nn.Linear(hidden_size1, hidden_size2)
+        self.ln2 = nn.LayerNorm(hidden_size2)
+
+        # The output layer
+        self.out = nn.Linear(hidden_size2, num_actions)
+
+        # Initialize layers according to DDPG paper
+        fan_in_uniform_init(self.linear1.weight)
+        fan_in_uniform_init(self.linear1.bias)
+
+        fan_in_uniform_init(self.linear2.weight)
+        fan_in_uniform_init(self.linear2.bias)
+
+        nn.init.uniform_(self.mu.weight, -final_bias, final_bias)
 
     def initialize_orthogonal(self):
         """
         This function initializes the weights of the network according to the method described in "Exact solutions to
         the nonlinear dynamics of learning in deep linear neural networks" - Saxe, A. et al. (2013)
         """
-        # Initialize the body
-        self.body.initialize_orthogonal()
 
-        # Initialize the actor
-        self.fc_control.apply(init_weights)
+        # Initialize linear1
+        self.linear1.apply(init_weights)
+
+        # Initialize linear2
+        self.linear2.apply(init_weights)
+
+        # Initialize output layer
+        self.out.apply(init_weights)
 
     def forward(self, state):
         """
         This function performs a forward pass through the network.
 
-        :param state:                   (tensor)    The input state the NN uses to compute an output.
-        :return means:                  (tensor)    The means used for selecting the action.
-        :return standard_deviations:    (tensor)    The standard deviations used for selecting the action.
+        :param state: (tensor)   The input state the NN uses to compute an output.
+        :return mu:   (tensor)   The output of the NN, which is the action to be taken.
         """
-        # Don't need to scale images because that is already handled by nature's forward
-        x = self.body.forward(state)
+        # Pass through layer 1
+        x = self.linear1(state)
+        x = self.ln1(x)
+        x = F.relu(x)
 
-        # Take the result and pass it through the new layer
-        x = self.fc_control(x)
+        # Pass through layer 2
+        x = self.linear2(x)
+        x = self.ln2(x)
+        x = F.relu(x)
 
-        # Break apart the output into means and standard deviations
-        means = x[0:1:1]
-        standard_deviations = x[2:]
+        # Pass through the output layer
+        x = self.out(x)
+
+        # Output is scaled using tanh as prescribed in DDPG paper
+        mu = torch.tanh(x)
 
         # Return the result
-        return means, standard_deviations
+        return mu
 
 
 ########################################################################################################################
 class CriticNN(nn.Module):
-    def __init__(self, input_size=4, hidden_size=6):
+    def __init__(self, num_inputs=4, hidden_size1=400, hidden_size2=300, num_actions=2, final_bias=3e-3):
         super(CriticNN, self).__init__()
         """
-        This Neural Network architecture creates a full critic, which estimates the value of a state.
+        This Neural Network architecture creates a full critic, which estimates the value of a state-action pair. The 
+        architecture is derived from the original DDPG paper.
 
-        :param input_size:  (int)   The desired size of the input layer. Should be the same size as the number of 
+        :param num_inputs:  (int)   The desired size of the input layer. Should be the same size as the number of 
                                         inputs to the NN. Default=4
-        :param hidden_size: (int)   The desired size of the hidden layers. Default=6
+        :param hidden_size1:(int)   The desired size of the first hidden layer. Default=400
+        :param hidden_size2:(int)   The desired size of the second hidden layer. Default=300
+        :param num_actions: (int)   The number of actions the network will recieve. Should be the same size as the 
+                                        number of outputs from the ActorNN. Default=2
+        :param final_bias:  (float) The final layers' weight and bias range for uniform distribution. Default=3e-3
         """
-        # Save the inputs
-        self.input_size = input_size
-        self.hidden_size = hidden_size
 
-        # Initialize the body
-        self.body = BodyNN(input_size=self.input_size, hidden_size=self.hidden_size)
+        # The first layer
+        self.linear1 = nn.Linear(num_inputs, hidden_size1)
+        self.ln1 = nn.LayerNorm(hidden_size1)
 
-        # Create the output layer
-        self.fc_value_function = nn.Sequential(
-            nn.Linear(in_features=self.hidden_size, out_features=1),
-            nn.ReLU
-        )
+        # The second layer
+        self.linear2 = nn.Linear(hidden_size1 + num_actions, hidden_size2)
+        self.ln2 = nn.LayerNorm(hidden_size2)
+
+        # The output layer
+        self.out = nn.Linear(hidden_size2, num_actions)
+
+        # Initialize layers according to DDPG paper
+        fan_in_uniform_init(self.linear1.weight)
+        fan_in_uniform_init(self.linear1.bias)
+
+        fan_in_uniform_init(self.linear2.weight)
+        fan_in_uniform_init(self.linear2.bias)
+
+        nn.init.uniform_(self.mu.weight, -final_bias, final_bias)
 
     def initialize_orthogonal(self):
         """
         This function initializes the weights of the network according to the method described in "Exact solutions to
         the nonlinear dynamics of learning in deep linear neural networks" - Saxe, A. et al. (2013)
         """
-        # Initialize the body
-        self.body.initialize_orthogonal()
 
-        # Initialize the critic
-        self.fc_value_function(init_weights)
+        # Initialize linear1
+        self.linear1.apply(init_weights)
 
-    def forward(self, state):
+        # Initialize linear2
+        self.linear2.apply(init_weights)
+
+        # Initialize output layer
+        self.out.apply(init_weights)
+
+    def forward(self, state, action):
         """
         This function performs a forward pass through the network.
 
-        :param state:                   (tensor)    The input state the NN uses to compute an output.
-        :return x:                  (tensor)    An estimate of the value of the input state.
+        :param state:   (tensor)    The input state for the NN to compute the state-action value.
+        :param action:  (tensor)    The input action for the NN to compute the state-action value.
+        :return q:      (tensor)    An estimated Q-value of the input state-action pair.
         """
-        # Don't need to scale images because that is already handled by nature's forward
-        x = self.body.forward(state)
+        # Pass through layer 1
+        x = self.linear1(state)
+        x = self.ln1(x)
+        x = F.relu(x)
 
-        # Take the result and pass it through the new layer
-        x = self.fc_value_function(x)
+        # Concatenate the first layer output with the action
+        x = torch.cat((x, action), 1)
+
+        # Pass through layer 2
+        x = self.linear2(x)
+        x = self.ln2(x)
+        x = F.relu(x)
+
+        # Pass through the output layer
+        q = self.out(x)
 
         # Return the result
-        return x
+        return q
 
