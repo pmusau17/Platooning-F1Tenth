99aa54dc52161c0cef1aeb47e6ea08a9e5eb9d5e
Patrick Musau <pmusau13ster@gmail.com>
2020-02-09 16:06:24 -0600

    Added Dave model

---
+++ src/f110-fall2018-skeletons/simulator/f1_10_sim/race/scripts/computer_vision/nn/conv/dave2.py
@@ -0,0 +1,64 @@
+#import the neccessary kpackages
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers.normalization import BatchNormalization
+from tensorflow.python.keras.layers.convolutional import Conv2D
+from tensorflow.python.keras.layers.convolutional import MaxPooling2D
+from tensorflow.python.keras.layers.core import Activation 
+from tensorflow.python.keras.layers.core import Flatten
+from tensorflow.python.keras.layers.core import Dropout
+from tensorflow.python.keras.layers.core import Dense
+from tensorflow.python.keras import backend as K
+from tensorflow.keras.utils import plot_model
+
+#Builds the DAVE2 Architechture described in: https://devblogs.nvidia.com/deep-learning-self-driving-cars/
+class DAVE2: 
+    @staticmethod
+    def build(height,width,depth,classes):
+        # initialize the model
+        model = Sequential()
+        #the shape of our image inputs
+        inputShape=(height, width, depth)
+        #if we are using "channels first" update the input shape
+        if (K.image_data_format()=="channels_first"):
+            inputShape=(depth, height, width)
+
+        #first set of CONV=>RELU=> POOL layers
+        model.add (Conv2D(24,(5,5),strides=(2,2), padding="valid",input_shape=inputShape))
+        model.add(Activation('relu'))
+
+        #second set of 5x5 CONV=>RELU=> POOL layers
+        model.add (Conv2D(36,(5,5), strides=(2,2),padding="valid"))
+        model.add(Activation('relu'))
+
+        #third set of 5x5 CONV=>RELU=> POOL layers
+        model.add (Conv2D(48,(5,5), strides=(2,2),padding="valid"))
+        model.add(Activation('relu'))
+
+        #first set of 3x3 CONV=>RELU=> POOL layers
+        model.add (Conv2D(64,(3,3), padding="valid"))
+        model.add(Activation('relu'))
+
+        #second set of 3x3 CONV=>RELU=> POOL layers
+        model.add (Conv2D(64,(3,3), padding="valid"))
+        model.add(Activation('relu'))
+
+        #set of fully connected layers
+        model.add(Flatten())
+        model.add(Dense(1164))
+        model.add(Activation('relu'))
+        model.add(Dense(100))
+        model.add(Activation('relu'))
+        model.add(Dense(10))
+        model.add(Activation('relu'))
+
+        #output
+        model.add(Dense(classes))
+        model.add(Activation('tanh'))
+
+        return model
+
+if __name__=="__main__":
+    model=DAVE2.build(66,200,3,1)
+    #This is responsible for constructing a graph based on the layers inside 
+    #The input model and then writing the graph to disk an image
+    plot_model(model, to_file="DAVE2.png",show_shapes=True)
