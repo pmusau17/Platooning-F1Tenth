641bef06b01dfb9150751f7b360c606dd602b240
Patrick Musau <pmusau13ster@gmail.com>
2020-02-10 17:45:34 -0600

    Data collection happening now

--- src/f110-fall2018-skeletons/simulator/f1_10_sim/race/scripts/computer_vision/synchronize_img_command.py
+++ src/f110-fall2018-skeletons/simulator/f1_10_sim/race/scripts/computer_vision/synchronize_img_command.py
@@ -28,6 +28,7 @@ class MessageSynchronizer:
         self.save_path_root=r.get_path('race')+'/scripts/computer_vision/data/'
         self.cv_bridge=CvBridge()
         self.count=0
+        self.save_count=0
 
         #create the time synchronizer
         self.sub = ApproximateTimeSynchronizer([self.image_rect_color,self.image_rect_color_cp,self.ackermann_stamped], queue_size = 20, slop = 0.049)
@@ -41,22 +42,28 @@ class MessageSynchronizer:
         try:
             cv_image=self.cv_bridge.imgmsg_to_cv2(image,"bgr8")
             self.count+=1
-
             # if you need to reshape the image uncomment the following line
             # cv_image=self.preprocess_image(cv_image,66,200)
         except CvBridgeError as e:
             print(e)
 
-        print(cv_image.shape,(ackermann_msg.drive.steering_angle,self.label_image(ackermann_msg.drive.steering_angle)),self.count)
+        #print(cv_image.shape,(ackermann_msg.drive.steering_angle,self.label_image(ackermann_msg.drive.steering_angle)),self.count)
 
+        #convert the steering command to a string to I can store it with the image name
+        #for efficient data storage
         command='%.10f' % ackermann_msg.drive.steering_angle
+        #replace the period with ~ so it's a valid filename
         command=command.replace('.','~')
-        #print(command)
-        print(self.save_path_root+self.label_image(ackermann_msg.drive.steering_angle)+'/'+str(rospy.Time.now())+'~'+command+'.jpg')
-        if(self.count % 10==-1):
-            cv2.imshow("Current Image",cv_image)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                exit()
+        
+        #save path 
+        save_path=self.save_path_root+self.label_image(ackermann_msg.drive.steering_angle)+'/'+str(rospy.Time.now())+'~'+command+'.jpg'
+        
+        #print(self.save_path_root+self.label_image(ackermann_msg.drive.steering_angle)+'/'+str(rospy.Time.now())+'~'+command+'.jpg')
+        
+        if(self.count % 7==0):
+            self.save_count+=0
+            self.save_image(cv_image,save_path)
+            
 
     #preprocess the images so that the DAEV model can use them
     #inspired by Adrian Rosebrock
@@ -85,22 +92,33 @@ class MessageSynchronizer:
         image=cv2.resize(image,(width,height))
         return image
 
-    #function that categorizes images into left,right,straight
+    #function that categorizes images into left, weak_left, straight, weak_right, right
     def label_image(self,steering_angle):
-        if(steering_angle<-0.0523599):
+        if(steering_angle<-0.20179):
             return "right"
-        elif(steering_angle>0.0523599):
+        elif(steering_angle>0.20179):
             return "left"
+        elif(steering_angle<-0.0523599 and steering_angle>-0.20179):
+            return "weak_right"
+        elif(steering_angle>0.0523599 and steering_angle<0.20179):
+            return "weak_left"
         else:
             return "straight"
 
     
     def save_image(self,image,path):
         dirPath = os.path.split(path)[0]
+        #print(dirPath)
         # if the output directory does not exist, create it
         if not os.path.exists(dirPath):
             os.makedirs(dirPath)
-        cv2.imwrite(image,path)
+            print('does not exist')
+
+        #cv2.imshow("Current Image",image)
+        #if cv2.waitKey(25) & 0xFF == ord('q'):
+        #        exit()
+        if not 'straight' in dirPath:
+            cv2.imwrite(path,image)
 
 if __name__=='__main__':
     rospy.init_node('image_command_sync')
