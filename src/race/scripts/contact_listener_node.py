512b1f65db7847c7fc419aa93f8d985e7e3f21d4
Patrick Musau <pmusau13ster@gmail.com>
2020-04-24 14:06:56 -0500

    Contact Listener

---
+++ src/race/scripts/contact_listener_node.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+from gazebo_msgs.msg import ContactsState
+import rospy
+import copy
+import numpy as np
+from std_srvs.srv import Empty
+
+""" This node will listen to the contact sensor and report when the car crashes into another object
+    There is also an option to reset the environment for reinforcement learning experiments
+"""
+
+class ContactSubscriber:
+
+
+    # constructor takes two parameters, the name of the racecar
+    # whether or not to reset the car on a crash event
+    def __init__(self,racecar_name,reset_on_crash=False):
+        # need to make sure that the services are correct
+        rospy.wait_for_service('/gazebo/reset_world')
+
+        self.reset_on_crash = reset_on_crash
+
+
+        self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_world', Empty)
+
+        self.contact_sub = rospy.Subscriber(racecar_name+'/contact_link_collisions',ContactsState,self.callback,queue_size=5)
+
+    def callback(self,msg):
+        states=msg.states
+        if(states):
+            rospy.logwarn("Collision Occured")
+            if(self.reset_on_crash):
+                self.reset_proxy()
+
+
+if __name__=="__main__": 
+
+    rospy.init_node("collision_node")
+    args = rospy.myargv()[1:]
+    
+    # get the racecar name
+    
+    racecar_name=args[0].replace('/','')
+
+    # reset world 
+    reset_world = args[1:]
+
+    if(reset_world):
+    # create object 
+        cs = ContactSubscriber(racecar_name,reset_on_crash=True)
+    else:
+        cs = ContactSubscriber(racecar_name)
+
+    rospy.spin()
+
+
+
+
+
+
