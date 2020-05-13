de7bed48dde2ea82ca316c4f52597afd9ba46b71
Patrick Musau <pmusau13ster@gmail.com>
2020-05-13 11:00:12 -0500

    Infrastructure changes on a_star_pure_pursuit

--- src/a_stars_pure_pursuit/scripts/waypoint_logger_pure_pursuit.py
+++ src/a_stars_pure_pursuit/scripts/waypoint_logger_pure_pursuit.py
@@ -3,6 +3,7 @@ import rospy
 import numpy as np
 import atexit
 import tf
+import os 
 from os.path import expanduser
 from time import gmtime, strftime
 from numpy import linalg as LA
@@ -11,46 +12,61 @@ from nav_msgs.msg import Odometry
 import rospkg 
 
 
-# get an instance of RosPack with the default search paths
-rospack = rospkg.RosPack()
-#get the path for this paackage
-package_path=rospack.get_path('a_stars_pure_pursuit')
-filename=package_path+'/waypoints/waypoints_1.csv'
 
+class WaypointLogger():
 
-file = open(filename, 'w')
+    def __init__(self,racecar_name):
+        self.racecar_name = racecar_name
 
-def save_waypoint(data):
-    quaternion = np.array([data.pose.pose.orientation.x, 
-                           data.pose.pose.orientation.y, 
-                           data.pose.pose.orientation.z, 
-                           data.pose.pose.orientation.w])
+        # use the rospack object to get paths
+        rospack = rospkg.RosPack()
+        # get the worldname from the parameter server
+        world_name = rospy.get_param("/world_name")
+        #get the path for this paackage
+        package_path=rospack.get_path('a_stars_pure_pursuit')
+        # get the pid to create "unique" filenames
+        self.filename=package_path+'/waypoints/{}_{}.csv'.format(world_name,os.getpid())
+        self.file = open(self.filename, 'w')
 
-    euler = tf.transformations.euler_from_quaternion(quaternion)
-    speed = LA.norm(np.array([data.twist.twist.linear.x, 
-                              data.twist.twist.linear.y, 
-                              data.twist.twist.linear.z]),2)
-    if data.twist.twist.linear.x>0.:
-        print data.twist.twist.linear.x
+    def save_waypoint(self,data):
+        quaternion = np.array([data.pose.pose.orientation.x, 
+                            data.pose.pose.orientation.y, 
+                            data.pose.pose.orientation.z, 
+                            data.pose.pose.orientation.w])
 
-    file.write('%f, %f, %f, %f, %f\n' % (data.pose.pose.position.x,
-                                     data.pose.pose.position.y,
-                                     euler[2],
-                                     speed,data.pose.pose.orientation.w))
+        euler = tf.transformations.euler_from_quaternion(quaternion)
+        speed = LA.norm(np.array([data.twist.twist.linear.x, 
+                                data.twist.twist.linear.y, 
+                                data.twist.twist.linear.z]),2)
+        if data.twist.twist.linear.x>0.:
+            print data.twist.twist.linear.x
 
-def shutdown():
-    file.close()
-    print('Goodbye')
+        self.file.write('%f, %f, %f, %f, %f\n' % (data.pose.pose.position.x,
+                                        data.pose.pose.position.y,
+                                        euler[2],
+                                        speed,data.pose.pose.orientation.w))
+
+    def shutdown(self):
+        self.file.close()
+        print('Goodbye')
  
-def listener():
-    rospy.init_node('waypoints_logger', anonymous=True)
-    rospy.Subscriber('/vesc/odom', Odometry, save_waypoint)
-    rospy.spin()
+    def listener(self):
+        rospy.init_node('waypoints_logger', anonymous=True)
+        rospy.Subscriber(self.racecar_name+'/odom', Odometry, self.save_waypoint)
+        rospy.spin()
 
 if __name__ == '__main__':
-    atexit.register(shutdown)
+    #get the arguments passed from the launch file
+    args = rospy.myargv()[1:]
+    
+    # get the path to the file containing the waypoints
+    racecar_name=args[0]
+
+    # create Waypoint Object
+    wp = WaypointLogger(racecar_name)
+    atexit.register(wp.shutdown)
     print('Saving waypoints...')
     try:
-        listener()
+        wp.listener()
     except rospy.ROSInterruptException:
         pass
