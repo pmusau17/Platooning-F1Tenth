d663cafd18c32ea8dc68f94d10ecddd40013f0aa
Patrick Musau <pmusau13ster@gmail.com>
2020-05-14 15:30:47 -0500

    pure pursuit refactoring

--- src/a_stars_pure_pursuit/README.md
+++ src/a_stars_pure_pursuit/README.md
@@ -1,36 +1,7 @@
 # Racecar-Pure_Pursuit-Planner
-ROS package for a motion planner used in head-to-head autonomous racing.
 
-The ROS package works coupled with the F110 repository for simulations. 
-Add the repository and follow the instructions for installing as given in the readme of the repository. 
+Implementation of the pure pursuit algorithm proposed by R. Craig Coulter in the paper ["Implementation of the Pure Pursuit Path Tracking Algorithm"](https://www.ri.cmu.edu/pub_files/pub3/coulter_r_craig_1992_1/coulter_r_craig_1992_1.pdf).
 
 
-git clone https://github.com/mlab-upenn/f110-fall2018-skeletons.git
-
-Clone the repository in the source folder of your ROS Workspace. 
-
-clone the a_stars_pure_pursuit in the same Source folder. 
-
-These dependencies are important to install the package: 
-
-1. CSV
-2. numpy
-3. Scipy
-5. clone the repository 
-6. Locate the scripts Locate the python scripts ‘pure_pursuit.py’ and make them executable using the
-following command.<br />
-	$ chmod +x “script name”<br />
-*if on compilation or launching the simulation, there is an error where the launch file is not able to locate the node of a python file, just locate that python file and make it executable by the above command<br />
-
-7. source the current setup file from the ROS workspace and make the workspace using command catkin_make<br />
-8. Roslaunch the package: <br />
-	$ roslaunch a_stars_pure_pursuit pure_pursuit_sim.launch<br />
-
-
-For any issues mail on sidsingh@seas.upenn.edu or Singh.sid930@gmail.com
-
-This is the reference video: 
-https://www.youtube.com/watch?v=UBTp22Loq4o&feature=youtu.be
-
-
-![](ppt.gif)
+# Documentation 
+loading ...
--- src/a_stars_pure_pursuit/scripts/pure_pursuit.py
+++ src/a_stars_pure_pursuit/scripts/pure_pursuit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import rospy
 from race.msg import drive_param
 from nav_msgs.msg import Odometry
@@ -24,6 +23,10 @@ class pure_pursuit:
 
         # pure pursuit parameters
         self.LOOKAHEAD_DISTANCE = 1.5#1.70 # meters
+        
+        # Distance from the 
+        self.distance_from_rear_wheel_to_front_wheel = 0.5
+
         self.VELOCITY = 3.2 # m/s
         self.goal = 0
         self.read_waypoints()
@@ -33,6 +36,7 @@ class pure_pursuit:
         # Publisher for the goal point
         self.goal_pub = rospy.Publisher(racecar_name+'/goal_point', MarkerArray, queue_size="1")
         self.considered_pub= rospy.Publisher(racecar_name+'/considered_points', MarkerArray, queue_size="1")
+        self.point_in_car_frame= rospy.Publisher(racecar_name+'/goal_point_car_frame', MarkerArray, queue_size="1")
         # Subscriber to vehicle position 
         rospy.Subscriber(racecar_name+"/odom", Odometry, self.callback, queue_size=1)
 
@@ -53,21 +57,26 @@ class pure_pursuit:
         self.path_points_y   = np.asarray([float(point[1]) for point in path_points])
         self.path_points_w   = np.asarray([float(point[2]) for point in path_points])
 
-        self.xy_points = np.hstack((self.path_points_x.reshape((-1,1)),self.path_points_y.reshape((-1,1))))
+        # list of xy pts 
+        self.xy_points = np.hstack((self.path_points_x.reshape((-1,1)),self.path_points_y.reshape((-1,1)))).astype('double')
    
-    def visualize_goal_point(self,pt,r=1.0,g=0.0,b=1.0):
+    def visualize_point(self,pts,publisher,frame='/map',r=1.0,g=0.0,b=1.0):
         # create a marker array
         markerArray = MarkerArray()
+
+        idx = np.random.randint(0,len(pts))
+        pt = pts[idx]
+
         x = float(pt[0])
         y = float(pt[1])
 		
         marker = Marker()
-        marker.header.frame_id = "/map"
+        marker.header.frame_id = frame
         marker.type = marker.SPHERE
         marker.action = marker.ADD
-        marker.scale.x = 0.1
-        marker.scale.y = 0.1
-        marker.scale.z = 0.1
+        marker.scale.x = 0.2
+        marker.scale.y = 0.2
+        marker.scale.z = 0.2
         marker.color.a = 1.0
         marker.color.r = r
         marker.color.g = g
@@ -77,7 +86,8 @@ class pure_pursuit:
         marker.pose.position.y = y
         marker.pose.position.z = 0
         markerArray.markers.append(marker)
-        self.goal_pub.publish(markerArray)
+        publisher.publish(markerArray)
+
 
     def visualize_considered_points(self,pts,r=0.0,g=0.0,b=1.0):
 
@@ -105,6 +115,7 @@ class pure_pursuit:
         marker.pose.position.y = y
         marker.pose.position.z = 0
         markerArray.markers.append(marker)
+            
 
         self.considered_pub.publish(markerArray)
 
@@ -121,92 +132,57 @@ class pure_pursuit:
 
         quaternion = (qx,qy,qz,qw)
         euler = euler_from_quaternion(quaternion)
-        yaw   = euler[2] 
+        yaw   = np.double(euler[2])
 
         x = data.pose.pose.position.x
         y = data.pose.pose.position.y
 
         ## finding the distance of each way point from the current position 
-        curr_pos=np.asarray([x,y]).reshape((1,2))
+        curr_pos= np.asarray([x,y]).reshape((1,2))
         dist_arr = np.linalg.norm(self.xy_points-curr_pos,axis=-1)
 
         ##finding those points which are less than the look ahead distance (will be behind and ahead of the vehicle)
-        #goal_arr = np.where((self.dist_arr < self.LOOKAHEAD_DISTANCE+0.3)&(self.dist_arr > self.LOOKAHEAD_DISTANCE-0.3))[0]
-        goal_arr = np.where(dist_arr < self.LOOKAHEAD_DISTANCE)[0]
+        goal_arr = np.where((dist_arr > self.LOOKAHEAD_DISTANCE) & (dist_arr<self.LOOKAHEAD_DISTANCE+0.3))[0]
         
         # finding the goal point which is within the goal points 
         pts = self.xy_points[goal_arr]
 
-        self.visualize_considered_points(pts)
-
-        
-
+        # get all points in front of the car, using the orientation 
+        # and the angle between the vectors
+        pts_infrontofcar=[]
         for idx in range(len(pts)): 
             v1 = pts[idx] - curr_pos
             #since the euler was specified in the order x,y,z the angle is wrt to x axis
             v2 = [np.cos(yaw), np.sin(yaw)]
 
-            temp_angle = self.find_angle(v1,v2)
-            #print(temp_angle)
-            if abs(temp_angle) < np.pi/2:
-                self.goal = idx
-                self.visualize_goal_point(pts[self.goal])
+            angle= self.find_angle(v1,v2)
+            if angle < np.pi/2:
+                pts_infrontofcar.append(pts[idx])
 
-    
-        #self.visualize_considered_points(pts)
-    
-        # # ##finding the goal point which is the last in the set of points less than the lookahead distance
-        # # ##if the closest points array could not be formed, then the point which is closest to the current position is the goal. 
-        # for idx in goal_arr:
-        #     #line from the point position to the car position
-        #     v1 = [self.path_points_x[idx]-x , self.path_points_y[idx]-y]
-
-        #     #since the euler was specified in the order x,y,z the angle is wrt to x axis
-        #     v2 = [np.cos(yaw), np.sin(yaw)]
-        #     #find the angle between these two vectors NOTE:These are in world coordinates
-        #     temp_angle = self.find_angle(v1,v2)
-        #     if abs(temp_angle) < np.pi/2:
-        #         self.goal = idx
-        #         #print("broke")
-        #         break
-
-        # ##finding the distance of the goal point from the vehicle coordinatesr
-
-        # L = self.dist_arr[self.goal]
-
-        # ##Transforming the goal point into the vehicle coordinate frame (This come straight from the paper)
-
-        # gvcx = self.path_points_x[self.goal] - x
-        # gvcy = self.path_points_y[self.goal] - y 
-        # goal_x_veh_coord = gvcx*np.cos(yaw) + gvcy*np.sin(yaw)
-        # goal_y_veh_coord = gvcy*np.cos(yaw) - gvcx*np.sin(yaw)
-
-        # # math: find the curvature and the angle 
-        # #alpha = self.path_points_w[self.goal] - (yaw)
-        # if temp_angle>np.pi/2:
-        #     temp_angle= yaw
-
-        # #print(temp_angle,self.path_points_w[self.goal])
-        # alpha = (temp_angle-yaw)
-        # k = 2 * math.sin(alpha)/L
-        # angle_i = math.atan(k*0.4)
-
-        # angle = angle_i*2
-        # angle = np.clip(angle, -0.6108652353, 0.6108652353) # 0.6108652353 radians = 35 degrees because car can only turn 35 degrees max
+        pts_infrontofcar =np.asarray(pts_infrontofcar)
+        # compute new distances
+        dist_arr = np.linalg.norm(pts_infrontofcar-curr_pos,axis=-1)- self.LOOKAHEAD_DISTANCE
         
-        # pt = [float(self.path_points_x[self.goal]),self.path_points_y[self.goal]]
-        # self.visualize_goal_point(pt)
+        # get the point closest to the lookahead distance
+        idx = np.argmin(dist_arr)
 
+        # goal point 
+        goal_point = pts_infrontofcar[idx]
+        self.visualize_point([goal_point],self.goal_pub)
+
+        
       
-        #self.set_speed(angle)
-        #self.const_speed(angle)
+        # transform it into the vehicle coordinates
+        v1 = (goal_point - curr_pos)[0].astype('double')
+        xgv = (v1[0] * np.cos(yaw)) + (v1[1] * np.sin(yaw))
+        ygv = (-v1[0] * np.sin(yaw)) + (v1[1] * np.cos(yaw))
 
-	    #publish the goal in the vehicle coordinates. 
-	    #goalPoint = Point(float(goal_x_veh_coord),float(goal_y_veh_coord),float(angle))
-        #pt = [float(goal_x_veh_coord),float(goal_y_veh_coord)]
-        #pt = [float(self.path_points_x[self.goal]),self.path_points_y[self.goal]]
-        #self.visualize_goal_point(pt)
-
+        vector = np.asarray([xgv,ygv])
+        self.visualize_point([vector],self.point_in_car_frame,frame='racecar/chassis',r=0.0,g=1.0,b=0.0)
+        
+        # calculate the steering angle
+        angle = math.atan2(ygv,xgv)
+        self.const_speed(angle)
    
     # USE THIS FUNCTION IF CHANGEABLE SPEED IS NEEDED
     def set_speed(self,angle):
@@ -242,9 +218,9 @@ class pure_pursuit:
 
     # find the angle bewtween two vectors    
     def find_angle(self, v1, v2):
-        cosang = np.dot(v1, v2)
-        sinang = la.norm(np.cross(v1, v2))
-        return np.arctan2(sinang, cosang)
+        cosang = np.dot(v1, v2).astype('double')
+        sinang = la.norm(np.cross(v1, v2)).astype('double')
+        return np.arctan2(sinang, cosang).astype('double')
 
 
 if __name__ == '__main__':
