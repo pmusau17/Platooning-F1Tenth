46d26016c669f59fb027384eefe454dc28389d6d
Patrick Musau <pmusau13ster@gmail.com>
2020-08-19 15:53:06 -0500

    This is as good a model for now

--- src/race/csv/vehicle_sys_id_script.m
+++ src/race/csv/vehicle_sys_id_script.m
@@ -1,81 +1,45 @@
 % load the data
-T = readtable('racecar_walker_clockwise_1.5.csv');
+exp1 = load_experiment_data('data_15870_0.csv',1);
+exp2 = load_experiment_data('data_15870_2.csv',1);
+exp3 = load_experiment_data('data_15870_3.csv',1);
+exp4 = load_experiment_data('data_15870_5.csv',1);
+exp5 = load_experiment_data('data_15870_6.csv',1);
+exp6 = load_experiment_data('data_15870_8.csv',1);
+exp7 = load_experiment_data('data_15870_9.csv',1);
 
-% boolean to enable plotting 
-ENABLE_PLOTS = false;
+% Merge the data
+data_est = merge(exp1,exp2,exp3,exp4,exp5,exp6,exp7);
 
-% The order of the data (time,x,y,speed,theta,u,delta)
-time = T{:,1};
-x = T{:,2}(157:400);
-y = T{:,3}(157:400);
-speed = T{:,4}(157:400);
-theta = T{:,5}(157:400);
-u = T{:,6}(157:400);
-delta = T{:,7}(157:400);
+num_experiments = 7;
 
-% create the input and output matrices
-
-% create the input and output matrices
-
-output = {x,y,speed,theta};
-output = cell2mat(output);
-
-
-input = {u,delta};
-input = cell2mat(input);
-
-% Create iddata object
-
-data = iddata(output,input,0.05);
-data.OutputName ={'x';'y';'speed';'theta'};
-data.InputName ={'u';'delta'};
-data.Intersample = {'foh';'foh'};
-
-
-if ENABLE_PLOTS
-% Let's look at the postion data we have
-figure();
-plot(x,y);
-xlabel('x') 
-ylabel('y') 
-title("Position of Vehicle")
-
-% Let's look at the speed and throttle data we have 
-figure();
-plot(time,speed);
-hold on;
-plot(time, u);
-xlabel('t') 
-ylabel('speed') 
-title("speed input of the car")
-
-% Let's look at the angle and input data we have 
-figure();
-plot(time,theta);
-hold on;
-plot(time, delta);
-xlabel('t') 
-ylabel('theta') 
-title("heading of car")
-end
-% Let's try the system identification now
 
 % specify the initial parameters
- ca = 1.633;
- cm = 0.2; 
- ch = 4.0;
- lf = 0.255;
- lr = 0.225;
+ ca = 1.1418;
+ cm = 0.0092; 
+ ch = -120.63;
+ lf = 0.225;%0.255;
+ lr = 0.225;%0.225;
+ parameters    = {ca,cm,ch,lf,lr};
+
+ % SysID options 
+opt = nlgreyestOptions;
+opt.Display = 'Full';
+opt.SearchOptions.FunctionTolerance = 1e-7;
+opt.SearchOptions.MaxIterations = 100;
 
 % not sure why they call it order
 % it species the number of model outputs
 % the model inputs, and states
 % so for us its 2 inputs, 4 outputs, 4 states
-
 order         = [4 2 4];
-parameters    = {ca,cm,ch,lf,lr};
-initial_states = [0; 0;0;0];
 Ts = 0;
+
+% Let's try the system identification now
+for i=1:num_experiments
+    
+% get the dataset   
+ds = getexp(data_est,i);
+initial_states = reshape(ds.y(1,:),[],1);
 nonlinear_model = idnlgrey('bicycle_model',order,parameters,initial_states,Ts);
 nonlinear_model.algorithm.SimulationOptions.Solver = 'ode45';
 nonlinear_model.algorithm.SimulationOptions.MaxStep = 1e-1;
@@ -84,17 +48,32 @@ nonlinear_model.SimulationOptions.AbsTol = 1e-6;
 nonlinear_model.SimulationOptions.RelTol = 1e-5;
 setpar(nonlinear_model,'Fixed',{false, false,false,true,true});
 
-% Do the estimation 
-opt = nlgreyestOptions;
-opt.Display = 'Full';
-opt.SearchOptions.FunctionTolerance = 1e-10;
-opt.SearchOptions.MaxIterations = 500;
-nonlinear_model = nlgreyest(data,nonlinear_model,opt);
 
-% get the parameters
-b_est = nonlinear_model.Parameters;
+nonlinear_model = nlgreyest(ds,nonlinear_model,opt);
 
-% compare the learned model to the data 
+params = nonlinear_model.Parameters;
+params = [params.Value];
+parameters = {params(1),params(2),params(3),lf,lr};
+
+
 figure();
-compare(data,nonlinear_model)
+compare(ds,nonlinear_model)
+end
+
+
+close all 
+% Let's see how the parameters do overall. 
+
+
+for i=1:num_experiments
+% get the dataset   
+ds = getexp(data_est,i);
+initial_states = reshape(ds.y(1,:),[],1);
+nonlinear_model = idnlgrey('bicycle_model',order,parameters,initial_states,Ts);
+figure();
+compare(ds,nonlinear_model)
+end 
+
+
+
 
