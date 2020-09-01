20ce341dcf8b72b6bba7591b36f58445444acd11
Patrick Musau <pmusau13ster@gmail.com>
2020-09-01 12:52:45 -0500

    get coordinates of the walls

---
+++ src/race/scripts/gen_map.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import OccupancyGrid
+from nav_msgs.srv import GetMap
+from nav_msgs.msg import OccupancyGrid
+import matplotlib.pyplot as plt
+
+
+import numpy as np
+
+class GenerateMap:
+
+    """Generates walls of maps:
+       Note: -1: Unknown
+              0: Free
+              100: Occupied """
+    def __init__(self,threshold=0.65):
+        rospy.wait_for_service('static_map')
+        self.srv_proxy = rospy.ServiceProxy('static_map', GetMap)
+        self.threshold = threshold
+        self.points =[]
+        self.x = []
+        self.y = []
+        self.x_free = []
+        self.y_free = []
+
+    def execute(self):
+        msg = self.srv_proxy().map 
+
+        # map metadata
+        origin =[msg.info.origin.position.x,msg.info.origin.position.y]
+        res = msg.info.resolution
+
+
+     
+
+        map_data= np.asarray(msg.data)
+        grid_size=(msg.info.width,msg.info.height)
+        map_data = map_data.reshape(grid_size)
+
+        occ_x,occ_y = np.where(map_data==100)
+        occ_x_free,occ_y_free = np.where(map_data==0)
+
+        for i in range(len(occ_x)):
+            x_point = res*occ_x[i] + origin[0]
+            y_point = res*occ_y[i] + origin[0]
+            self.points.append((x_point,y_point))
+            self.x.append(x_point)
+            self.y.append(y_point)
+
+        for i in range(len(occ_x_free)):
+            x_point = res*occ_x_free[i] + origin[0]
+            y_point = res*occ_y_free[i] + origin[0]
+            self.x_free.append(x_point)
+            self.y_free.append(y_point)
+            
+        
+        plt.plot(self.x,self.y,'ro')
+        plt.plot(self.x_free,self.y_free,'bo')
+        plt.title('Plot of obstacles in: ')
+        plt.ylabel('x')
+        plt.xlim([-15,15])
+        plt.ylim([-15,15])
+        plt.xlabel('y')
+        plt.show()
+
+
+if __name__=="__main__":
+    rospy.init_node("getMap")
+    gm = GenerateMap()
+    gm.execute()
