e762344f6a6719750c37ddabcb68960cc7c6ae76
Patrick Musau <pmusau13ster@gmail.com>
2020-10-15 12:01:29 -0500

    change plots

--- src/race/sys_id/validate_experiment.m
+++ src/race/sys_id/validate_experiment.m
@@ -20,22 +20,14 @@ err = immse(x,xe);
 
 fig= figure();
 set(gcf, 'Position',  [100, 100, 900, 900])
-subplot(2,2,1);
-plot(t,x(3,:),'DisplayName','ground-truth,gazebo')
-hold on;
-plot(t,xe(3,:),'DisplayName','sys-id model')
-xlabel('t') 
-ylabel('linear speed') 
-title("Experiment 1: Linear Speed of Vehicle")
-legend
 
-subplot(2,2,2);
+subplot(2,2,[1,2]);
 plot(t,x(4,:),'DisplayName','ground-truth,gazebo')
 hold on;
 plot(t,xe(4,:),'DisplayName','sys-id model')
-xlabel('t') 
+xlabel('t (seconds)') 
 ylabel('Vehicle heading (radians)') 
-title("Experiment 1: Vehicle heading")
+title("Vehicle heading")
 legend
 
 
@@ -46,7 +38,7 @@ plot(xe(1,:),xe(2,:),'DisplayName','sys-id model')
 hold off;
 xlabel('x') 
 ylabel('y') 
-title("Experiment 1: Position of Vehicle")
+title("Position of Vehicle")
 legend
 sgtitle(strcat('State-Vector MSE=',string(err)))
 figname = split(strrep(csv_filename,'csv/',''),".");
