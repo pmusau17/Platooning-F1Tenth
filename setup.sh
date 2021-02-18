afde4c85c881a5ad7775d8125bcd523952e34f69
Patrick Musau <pmusau13ster@gmail.com>
2021-02-18 09:16:40 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 sudo apt-get update
 sudo apt-get install -y ros-kinetic-ros-control ros-kinetic-ros-controllers ros-kinetic-gazebo-ros-control ros-kinetic-ackermann-msgs ros-kinetic-joy
 sudo apt-get install -y ros-kinetic-teb-local-planner ros-kinetic-move-base ros-kinetic-navigation ros-kinetic-hector-slam ros-kinetic-driver-common ros-kinetic-actionlib 
+sudo apt-get install -y ros-kinetic-pcl-conversions ros-kinetic-pcl-msgs ros-kinetic-pcl-ros libeigen3-dev libproj-dev libmove-base-msgs-dev
+sudo ln -s /usr/include/eigen3/Eigen /usr/local/include/Eigen
 
 pip install rospkg
 pip install defusedxml
