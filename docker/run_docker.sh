97a9da6c920294f4bc02ef1c25ec5649426e4417
pmusau17 <pmusau13ster@gmail.com>
2021-03-08 13:34:33 -0600

    launch

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -2,4 +2,4 @@
 
 
 #run the docker container
-docker container run --runtime=nvidia -it -e DISPLAY  --rm --net=host --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix platoon_test bash
+docker container run --runtime=nvidia -it -e DISPLAY  --rm --net=host --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix simulator bash
