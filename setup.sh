cbc1ff8021dcb9ea13c2e71a8d1c525ee0bbb4c0
Patrick Musau <pmusau13ster@gmail.com>
2021-06-25 15:35:59 -0500

    clean up noetic port

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
 #install all of the relevant ROS packages needed to run the simulator
-sudo apt-get update
-sudo apt-get install -y ros-kinetic-ros-control ros-kinetic-ros-controllers ros-kinetic-gazebo-ros-control ros-kinetic-ackermann-msgs ros-kinetic-joy
-sudo apt-get install -y ros-kinetic-teb-local-planner ros-kinetic-move-base ros-kinetic-navigation ros-kinetic-hector-slam ros-kinetic-driver-common ros-kinetic-actionlib 
-sudo apt-get install -y ros-kinetic-pcl-conversions ros-kinetic-pcl-msgs ros-kinetic-pcl-ros libeigen3-dev libproj-dev libmove-base-msgs-dev
-sudo ln -s /usr/include/eigen3/Eigen /usr/local/include/Eigen
+rodep update; rosdep install -i --from-path src -y
 
-pip install rospkg
-pip install defusedxml
-pip install PySide2
-pip uninstall em
-pip install empy 
-pip install imutils
-pip install -r requirements-gpu.txt
+pip install pycryptodomex
