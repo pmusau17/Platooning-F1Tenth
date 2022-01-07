4defa066d1713fd79d05084840c363c3f8313a8b
Patrick Musau <pmusau13ster@gmail.com>
2022-01-07 09:33:16 -0600

    just use the message filters

--- src/race/scripts/lidar_to_cartesian.py
+++ src/race/scripts/lidar_to_cartesian.py
@@ -15,27 +15,53 @@ from race.msg import angle_msg
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
 from std_msgs.msg import String
+from nav_msgs.msg import Odometry
+#need to subscribe to the steering message and angle message
+from message_filters import ApproximateTimeSynchronizer, Subscriber
 
 
-from tf2_ros import *
-import tf2_geometry_msgs
+use_tf2 = False
+
+# There are some python importing issues in noetic if you have anaconda 
+# installed, deactivate anaconda if you want to use the tf2 python libraries
+
+if(use_tf2):
+    from tf2_ros import *
+    import tf2_geometry_msgs
+else:
+    import tf
 
 
 
 class LidarToCartestian:
 
-    def __init__(self,racecar_name):
+    def __init__(self,racecar_name,use_tf2):
 
-        # setting up the tf2 transforms 
-        self.tfBuffer = Buffer()
-        self.listener = TransformListener(self.tfBuffer)
+
+        if(use_tf2):
+            # setting up the tf2 transforms 
+            self.tfBuffer = Buffer()
+            self.listener = TransformListener(self.tfBuffer)
         self.racecar_name = racecar_name
-        self.vis_pub = rospy.Publisher('lidar_to_cartesian', MarkerArray,queue_size=100)
+        self.vis_pub = rospy.Publisher('lidar_to_cartesian', MarkerArray,queue_size=1)
+
+        self.odom_msg = None
+        
 
         # this was a massive headache, need to let the publisher register with the master before we start using the 
         # publisher in the subscirber callback
         rospy.sleep(0.5)
-        self.sub = rospy.Subscriber(racecar_name+'/scan', LaserScan, self.lidar_callback)
+        if(use_tf2):
+            self.sub = rospy.Subscriber(racecar_name+'/scan', LaserScan, self.lidar_callback_tf)
+        else:
+            self.sub = Subscriber(racecar_name+'/scan', LaserScan)
+            self.odom_sub = Subscriber(racecar_name+'/odom', Odometry)
+
+            #create the time synchronizer
+            self.main_sub = ApproximateTimeSynchronizer([self.sub,self.odom_sub], queue_size = 20, slop = 0.019)
+            #register the callback to the synchronizer
+            self.main_sub.registerCallback(self.lidar_callback)
+        
         
     
     # I won't use this now but this works too
@@ -74,7 +100,67 @@ class LidarToCartestian:
     def is_left(self,p1,p2,p3):
         return ((p2.point.x - p1.point.x)*(p3.point.y - p1.point.y) - (p2.point.y - p1.point.y)*(p3.point.x - p1.point.x)) > 0
     
-    def lidar_callback(self,data):
+    def lidar_callback(self,scan_data,pose_msg):
+        
+        
+        position_x = pose_msg.pose.pose.position.x
+        position_y = pose_msg.pose.pose.position.y
+
+        quaternion = np.array([pose_msg.pose.pose.orientation.x,
+                            pose_msg.pose.pose.orientation.y,
+                            pose_msg.pose.pose.orientation.z,
+                            pose_msg.pose.pose.orientation.w])
+
+        heading_angle = tf.transformations.euler_from_quaternion(quaternion)[2]
+
+        ranges = np.asarray(scan_data.ranges)
+        points = []
+        markerArray = MarkerArray()
+        for index, lidar_range in enumerate(ranges):
+        
+            angle=(index-540)/4.0
+            rad=(angle*math.pi)/180
+            laser_beam_angle = rad
+
+
+
+            rotated_angle = laser_beam_angle + heading_angle
+
+            # the 0.265 is the lidar's offset along the x-axis of the car
+            # it's in the xacro file
+            x_coordinate = (lidar_range) * math.cos(rotated_angle) + position_x + 0.265*math.cos(heading_angle)
+            y_coordinate = (lidar_range) * math.sin(rotated_angle) + position_y + 0.265*math.sin(heading_angle)
+            #points.append(CartesianPoint(x_coordinate, y_coordinate))
+            points.append([x_coordinate,y_coordinate])
+
+            marker = Marker()
+            marker.header.frame_id = "map"
+            marker.header.stamp = rospy.Time.now() 
+            marker.id = index
+            marker.type = marker.SPHERE
+            marker.action = marker.ADD
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
+
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+            
+                    
+            marker.pose.orientation.w = 1.0
+            marker.lifetime = rospy.Duration(0.1)
+          
+            marker.pose.position.x = x_coordinate
+            marker.pose.position.y = y_coordinate
+            marker.pose.position.z = -0.075
+            markerArray.markers.append(marker)
+        self.vis_pub.publish(markerArray)
+
+
+    # conversion using tf tree (this is very slow)
+    def lidar_callback_tf(self,data):
 
         ranges=data.ranges
 
@@ -157,5 +243,5 @@ if __name__ == "__main__":
     racecar_name=args[0]
 
     rospy.init_node('lidar_to_cartesian', anonymous=True)
-    ltc = LidarToCartestian(racecar_name)
+    ltc = LidarToCartestian(racecar_name,use_tf2)
     rospy.spin()
